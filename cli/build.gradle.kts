7f6b018b9bc77fb970122bec5788c4c43c73dc1b
Aniruddh Fichadia <Ani.Fichadia@gmail.com>
2024-07-18 23:35:39 +1000

    Ensures fatjar webp works
    Merges manfests, especially scrimage-webp, otherwise webp conversion would fail

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.shadow)
@@ -19,4 +21,8 @@ dependencies {
 
 application {
     mainClass.set("com.anifichadia.figmaimporter.cli.MainKt")
+}
+
+tasks.withType<ShadowJar> {
+    mergeServiceFiles()
 }
