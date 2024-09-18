9a290566448c48c27575e0ba018500d93691d12d
Aniruddh Fichadia <Ani.Fichadia@gmail.com>
2024-09-18 22:43:28 +1000

    Removes SVG to AVD conversion retries

--- library-android/src/main/java/com/anifichadia/figstract/android/importer/asset/model/importing/ImportPipelineSteps.kt
+++ library-android/src/main/java/com/anifichadia/figstract/android/importer/asset/model/importing/ImportPipelineSteps.kt
@@ -5,7 +5,6 @@ import com.anifichadia.figstract.importer.asset.model.importing.ImportPipeline
 import com.anifichadia.figstract.importer.asset.model.importing.ImportPipeline.Output.Companion.single
 import com.anifichadia.figstract.util.FileManagement
 import io.ktor.utils.io.core.toByteArray
-import kotlinx.coroutines.delay
 import java.io.ByteArrayOutputStream
 
 /**
@@ -22,37 +21,17 @@ val androidSvgToAvd = ImportPipeline.Step("androidSvgToAvd()") { instruction, in
     )
     tempOutputPath.toFile().writeBytes(input.data)
 
-    // TODO: when writing to file, there's a delay flushing the file??? which causes the following steps to fail cause the file doesn't exist
-    var attemptCount = 0
-    val attemptLimit = 20
-    val attemptDelay = 50L
+    val outputStream = ByteArrayOutputStream()
+    Svg2Vector.parseSvgToXml(tempOutputPath, outputStream)
 
-    var failure: Throwable?
-    do {
-        attemptCount += 1
-        delay(attemptDelay)
-
-        try {
-            val outputStream = ByteArrayOutputStream()
-            Svg2Vector.parseSvgToXml(tempOutputPath, outputStream)
-
-            return@Step input
-                .copy(
-                    data = outputStream.toByteArray(),
-                    target = input.target.copy(
-                        format = "xml",
-                    ),
-                )
-                .single()
-        } catch (e: Throwable) {
-            failure = e
-        }
-    } while (attemptCount <= attemptLimit)
-
-    // TODO logger this
-    println("${instruction.export.nodeId} ${instruction.import.importTarget.outputName}: $failure")
-
-    throw failure ?: error("whoops")
+    input
+        .copy(
+            data = outputStream.toByteArray(),
+            target = input.target.copy(
+                format = "xml",
+            ),
+        )
+        .single()
 }
 
 val androidVectorColorToPlaceholder = ImportPipeline.Step("androidVectorColorToPlaceholder()") { _, input ->
