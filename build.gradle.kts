c5ed9a571ded9aa1511ab3f82d1f7984b1f154e9
Aniruddh Fichadia <Aniruddh.Fichadia@anz.com>
2025-01-15 14:19:47 +1100

    Prepares release 0.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.plugin.extraProperties
 import java.util.*
 
-val VERSION = "0.0.3"
+val VERSION = "0.0.4"
 
 // Make sure these are always in sync
 val javaVersion = JavaVersion.VERSION_17
