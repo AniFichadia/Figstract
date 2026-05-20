b313816adf937be5f83908f3fa0473e7be6b3cf9
Aniruddh Fichadia <Ani.Fichadia@gmail.com>
2026-05-20 15:30:56 +1000

    Adds handling for new too many images error from Figma

--- library-core/src/main/java/com/anifichadia/figstract/figma/api/KnownErrors.kt
+++ library-core/src/main/java/com/anifichadia/figstract/figma/api/KnownErrors.kt
@@ -6,10 +6,12 @@ import com.anifichadia.figstract.figma.model.Error
 object KnownErrors {
     val rateLimitExceeded = Error(429, "Rate limit exceeded")
 
-    suspend fun <R> ApiResponse<R>.errorMatches(error: Error): Boolean {
+    suspend fun <R> ApiResponse<R>.errorMatches(error: Error, vararg errors: Error): Boolean {
+        val allErrors = listOf(error, *errors)
         return if (this is ApiResponse.Failure.ResponseError) {
             runCatching {
-                this.errorBodyAsOrNull<Error>() == error
+                val errorBody = this.errorBodyAsOrNull<Error>()
+                allErrors.any { error -> errorBody == error }
             }.getOrElse { false }
         } else {
             false
